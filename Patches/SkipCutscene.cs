09f49efc51cfb0d5fd39a932893ba7a5f965e29d
Vitaxses <151165512+Vitaxses@users.noreply.github.com>
2025-12-27 21:52:09 +0100

    Rework SkipCutscene.cs & make credits skippable

--- Patches/SkipCutscene.cs
+++ Patches/SkipCutscene.cs
@@ -3,18 +3,18 @@ namespace QoL.Patches;
 [HarmonyPatch(typeof(InputHandler), nameof(InputHandler.SetSkipMode))]
 internal static class InputHandlerPatch
 {
-    private static readonly string[] UnskipScene = { "Bone_East_Umbrella", "Belltown", "Room_Pinstress", "Belltown_Room_pinsmith", "Belltown_Room_doctor", "End_Credits_Scroll", "End_Credits", "Menu_Credits", "End_Game_Completion", "PermaDeath", "Bellway_City", "City_Lace_cutscene" };
+    private static readonly string[] UnskipScene = { "Bone_East_Umbrella", "Belltown", "Room_Pinstress", "Belltown_Room_pinsmith", 
+        "Belltown_Room_doctor", "End_Credits_Scroll", "End_Credits", "Menu_Credits", "End_Game_Completion", 
+        "PermaDeath", "Bellway_City", "City_Lace_cutscene", "Opening_Sequence_Act3" };
 
     [HarmonyWrapSafe, HarmonyPrefix]
     private static bool Prefix(InputHandler __instance, ref GlobalEnums.SkipPromptMode newMode)
     {
-        if (!Configs.SkipCutscene.Value || UnskipScene.Contains(GameManager.instance.sceneName)) 
-            return true;
-
-        __instance.SkipMode = newMode = GlobalEnums.SkipPromptMode.SKIP_INSTANT;
-        __instance.skipCooldownTime = 0f;
-        __instance.readyToSkipCutscene = true;
-        return false;
+        if (Configs.SkipCutscene.Value && !UnskipScene.Contains(GameManager.instance.sceneName))
+        {
+            newMode = GlobalEnums.SkipPromptMode.SKIP_INSTANT;
+        }
+        return true;
     }
 }
 
@@ -22,14 +22,30 @@ internal static class InputHandlerPatch
 internal static class SkippableSequencePatch
 {
     [HarmonyWrapSafe, HarmonyPrefix]
-    private static bool Prefix(SkippableSequence __instance, ref bool __result)
+    private static bool Prefix(SkippableSequence __instance)
+    {
+        if (Configs.SkipCutscene.Value)
+            __instance.AllowSkip();
+        
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(CreditsHelper), nameof(CreditsHelper.Start))]
+internal static class CreditsHelperPatch
+{
+
+    [HarmonyWrapSafe, HarmonyPrefix]
+    static void Prefix_Start(CreditsHelper __instance)
     {
         if (!Configs.SkipCutscene.Value)
-            return true;
-        
-        __instance.AllowSkip();
-        __instance.canSkip = __result = true;
-        return false;
+            return;
+
+        CutsceneHelper cutSceneHelper = __instance.cutSceneHelper;
+        cutSceneHelper.startSkipLocked = false;
+        __instance.startPause = cutSceneHelper.waitBeforeFadeIn = 0f;
+        cutSceneHelper.skipMode = GlobalEnums.SkipPromptMode.SKIP_INSTANT;
+        cutSceneHelper.UnlockSkip();
     }
 }
 
